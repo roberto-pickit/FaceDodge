1c05897eda6d927d63b655f9921a9d235ba1f357
roberto-pickit <roberto.lacommare@pickit3d.com>
2024-02-26 10:01:44 +0100

    main code

---
+++ Game/gamecomponents/gamecomp.py
@@ -0,0 +1,110 @@
+import pygame
+from Game.gamecomponents.constants import *
+#Vision stuff
+# import the necessary packages
+from EyeTracking.pyimagesearch.eyetracker import EyeTracker
+from EyeTracking.pyimagesearch import imutils
+import cv2
+
+
+# Initialize camera
+
+# construct the eye tracker
+et = EyeTracker("EyeTracking/cascades/haarcascade_frontalface_default.xml","EyeTracking/cascades/haarcascade_eye.xml")
+
+
+# Square class
+class Square:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, RED, pygame.Rect(self.x, self.y, self.size, self.size))
+
+    def move(self, dx):
+        self.x += dx
+        if self.x <= 0:
+            self.x = 0
+        elif self.x >= WIDTH - SQUARE_SIZE:
+            self.x = WIDTH - SQUARE_SIZE
+
+# Obstacle class
+class Obstacle:
+    def __init__(self, x, y, width, height,speedmult):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.speedmult= speedmult
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, BLUE, pygame.Rect(self.x, self.y, self.width, self.height))
+
+    def move(self, speed):
+        self.y += speed*self.speedmult
+
+    def off_screen(self):
+        return self.y > HEIGHT
+
+    def hits(self, square):
+        return (square.x < self.x + self.width and square.x + square.size > self.x and
+                square.y < self.y + self.height and square.y + square.size > self.y)
+
+class Benefit(Obstacle):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height,1)
+    def draw(self, surface):
+        pygame.draw.circle(surface, GOLD, (self.x, self.y), self.height/2)
+
+
+# Score class
+class Score:
+    def __init__(self,num_lives=3,num_obst_survived=0):
+        self.num_lives = num_lives
+        self.num_obst_survived= num_obst_survived
+        self.myfont = pygame.font.SysFont(None, 30)
+
+
+    def draw(self, screen):
+        textsurface = self.myfont.render('Score: ' + str(self.num_obst_survived)+' Lives: ' + str(self.num_lives), False, (0, 0, 0))
+        screen.blit(textsurface, (50, 50))  # adjust coordinates as needed
+        
+    def updateScore(self, obst, death=0):
+        self.num_obst_survived+=obst
+        self.num_lives+=death
+        if obst<0:
+            self.num_obst_survived=0
+
+
+def faceTracking(camera):
+        (grabbed, frame) = camera.read()
+        if not grabbed:
+            print("Unable to access camera")
+            exit(1)
+        frame = imutils.resize(frame, width = 300) 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect faces and eyes in the image
+        detections = et.track(gray)
+
+        rectface= detections["face"]
+        if len(rectface)!=0: 
+            obst_move=OBST_SPEED
+            cv2.rectangle(frame, (rectface[0], rectface[1]), (rectface[2], rectface[3]), (0, 255, 0), 2)
+            if (rectface[0]+rectface[2]/2>270):
+                dx = -SPEED*2
+            elif(rectface[0]+rectface[2]/2>220):
+                dx = -SPEED
+            elif(rectface[0]+rectface[2]/2<130):
+                dx = SPEED*2
+            elif (rectface[0]+rectface[2]/2<180):
+                dx = SPEED
+            else:
+                dx= 0
+        else:
+            obst_move= 0
+            dx=0
+        frame = cv2.flip(frame, 1)
+        cv2.imshow("Video streaming", frame)
+        return dx, obst_move
---
+++ main.py
@@ -0,0 +1,95 @@
+import pygame
+#Vision stuff
+# import the necessary packages
+from EyeTracking.pyimagesearch.eyetracker import EyeTracker
+import cv2
+
+import random
+from Game.gamecomponents.gamecomp import Obstacle, Benefit, Square, Score, faceTracking
+from Game.gamecomponents.constants import *
+
+
+# Initialize Pygame
+pygame.init()
+pygame.font.init()
+
+
+
+# Set up the game
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+square = Square(WIDTH // 2, HEIGHT - SQUARE_SIZE - 10, SQUARE_SIZE)
+obstacles = []
+benefits = []
+lives= 3
+score= Score(lives,0)
+clock = pygame.time.Clock()
+dx=0
+obst_move=0
+camera = cv2.VideoCapture(0)
+
+while True:
+    (dx, obst_move)=faceTracking(camera)
+        
+         
+    screen.fill(WHITE)
+    square.move(dx)
+    square.draw(screen)
+    score.draw(screen)
+    if random.randint(0, 50) < 3 & obst_move>0 :  # 2% chance to create a new obstacle every frame
+        obstacles.append(Obstacle(random.randint(0, WIDTH - OBSTACLE_WIDTH), -OBSTACLE_HEIGHT, 
+                                  OBSTACLE_WIDTH*random.randint(10,500)/100, OBSTACLE_HEIGHT,random.randint(1,3)))
+    if random.randint(0, 500) < 2 & obst_move>0 :  # 2% chance to create a new obstacle every frame
+        benefits.append(Benefit(random.randint(0, WIDTH - OBSTACLE_WIDTH), -OBSTACLE_HEIGHT, OBSTACLE_WIDTH, OBSTACLE_HEIGHT))
+    
+    for benefit in benefits:
+        benefit.move(obst_move)
+        benefit.draw(screen)
+        if benefit.off_screen():
+            benefits.remove(benefit)
+        if benefit.hits(square):
+            score.updateScore(0,1)
+            benefits.remove(benefit)
+
+
+    for obstacle in obstacles:
+        obstacle.move(obst_move)
+        obstacle.draw(screen)
+        if obstacle.off_screen():
+            obstacles.remove(obstacle)
+            score.updateScore(1)
+        if obstacle.hits(square):  # game over if an obstacle hits the square
+            #obstacles = []
+            obstacles.remove(obstacle)
+            score.updateScore(0,-1)
+            square = Square(WIDTH // 2, HEIGHT - SQUARE_SIZE - 10, SQUARE_SIZE)
+
+    pygame.display.flip()
+    clock.tick(120)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    if score.num_lives<1:
+        pygame.QUIT
+        pygame.quit()
+
+        print ("GAME OVER")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or score.num_lives<1:
+            pygame.quit()
+            print ("GAME OVER")
+
+            camera.release()
+            cv2.destroyAllWindows()
+        
+        
+        
+camera.release()
+cv2.destroyAllWindows()
+        
+    
+    #elif keys[pygame.K_LEFT]:
+    #    dx = -SPEED
+    #elif keys[pygame.K_RIGHT]:
+    #    dx = SPEED
+    #else:
+    #    dx=0
+  
